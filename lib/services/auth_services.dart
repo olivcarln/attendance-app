5361e6f8db47152fdb1fd0c7120693d24714ec75
mac <mac@Carle.local>
2025-11-12 13:16:25 +0700

    adding dependencies

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class AuthServices {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  AuthServices() {
+    if (!kIsWeb) {
+      //only for android or ios
+      FirebaseAuth.instance.setSettings(
+        appVerificationDisabledForTesting: true,
+        forceRecaptchaFlow: false,
+      );
+    }
+  }
+  // get current user
+  User? get currentUser => _auth.currentUser;
+
+  // auth state changes stream
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // sign in with email adn password
+  Future<UserCredential> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
